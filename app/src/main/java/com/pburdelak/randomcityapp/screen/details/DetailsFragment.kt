580e32ec0f1764e9695cdef38da0a9d5d376eb79
Przemysław Burdelak <przemek@codeaddict.io>
2021-05-06 20:45:19 +0200

    [Feature] RCA | Google maps integration

--- app/src/main/java/com/pburdelak/randomcityapp/screen/details/DetailsFragment.kt
+++ app/src/main/java/com/pburdelak/randomcityapp/screen/details/DetailsFragment.kt
@@ -1,16 +1,26 @@
 package com.pburdelak.randomcityapp.screen.details
 
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.addCallback
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 import com.pburdelak.randomcityapp.databinding.FragmentDetailsBinding
 import com.pburdelak.randomcityapp.screen.base.BaseFragment
+import com.pburdelak.randomcityapp.utils.log
 import dagger.hilt.android.AndroidEntryPoint
+import java.lang.Exception
+import java.util.*
 
 @AndroidEntryPoint
-class DetailsFragment: BaseFragment<FragmentDetailsBinding>() {
+class DetailsFragment: BaseFragment<FragmentDetailsBinding>(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +36,59 @@ class DetailsFragment: BaseFragment<FragmentDetailsBinding>() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        binding.root.onCreate(savedInstanceState)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.root.getMapAsync(this)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.root.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.root.onLowMemory()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.root.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.root.onStart()
+    }
+
+    override fun onPause() {
+        binding.root.onPause()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        binding.root.onStop()
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        binding.root.onDestroy()
+        super.onDestroyView()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        try {
+            val address = Geocoder(requireContext()).getFromLocationName("Poznan", 1)[0]
+            val latLng = LatLng(address.latitude, address.longitude)
+            val update = CameraUpdateFactory.newLatLngZoom(latLng, 10f)
+            map.moveCamera(update)
+        } catch (exception: Exception) {
+            exception.log()
+            Toast.makeText(requireContext(), exception.localizedMessage, Toast.LENGTH_LONG).show()
+        }
     }
 }
